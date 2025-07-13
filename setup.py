48b0537687f0543e34249efea0433f355fab9684
zshnrg <ghosanirozan1@gmail.com>
2025-07-13 17:28:59 +0700

    chore: bumb up version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pybmdope",
-    version="0.0.4",
+    version="1.0.0",
     author="Rozan Ghosani",
     author_email="ghosanirozan1@gmail.com",
     description="A Block Metadata-Driven Order-Preserving Encryption (BMDOPE)",
