b42cc8e965fb1b7ea1355676aa4055d3c8ecdb91
zshnrg <ghosanirozan1@gmail.com>
2025-07-23 06:00:43 +0700

    chore: bump version to 1.1.0

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name="pybmdope",
-    version="1.0.1",
+    version="1.1.0",
     author="Rozan Ghosani",
     author_email="ghosanirozan1@gmail.com",
     description="A Block Metadata-Driven Order-Preserving Encryption (BMDOPE)",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/zshnrg/pybmdope",
-    packages=find_packages(),  # ← looks for pybmdope/ by default
+    packages=find_packages(),
     python_requires=">=3.6",
     install_requires=["cryptography>=41.0.0"],
     classifiers=[
